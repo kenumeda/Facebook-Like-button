c144a0aa12d0ffa155b6f12172a5a2bb3c372a10
kenumeda <108510299+kenumeda@users.noreply.github.com>
2023-05-12 23:34:09 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Facebook-Like-button
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-tw6yx2)
---
+++ index.js
@@ -0,0 +1,7 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>Facebook Like Button</h1>
+<input type="radio">Like this</input>`;
