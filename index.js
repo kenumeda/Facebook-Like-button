ded00561a71983123ee6a062f99305dd651c5289
kenumeda <108510299+kenumeda@users.noreply.github.com>
2023-05-13 22:53:47 +1000

    1 file modified

--- index.js
+++ index.js
@@ -3,5 +3,4 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>Facebook Like Button</h1>
-<input type="radio">Like this</input>`;
+appDiv.innerHTML = `<h1>Facebook Like Button</h1>`
